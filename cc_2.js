ebc67eb31ba6d0c7c52542f87985068f70a3d611
Justin Wong <jcwong5@usf.edu>
2025-09-05 12:44:52 -0400

    Inital commit for coding challenge 02a

---
+++ cc_2.js
@@ -0,0 +1,18 @@
+// Coding Challenge 02.a
+
+const productName = "basketball";
+let costPerUnit = 7.50;
+let basePrice = 10.00;
+let discountRate = 0.12;
+let salesTaxRate = 0.05;
+let fixedMonthlyCosts = 20;
+
+let discountPrice = basePrice * (1- discountRate);
+let finalPriceWithTax = discountPrice * (1 + salesTaxRate);
+let profitPerUnit = finalPriceWithTax - costPerUnit;
+let breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit);
+let isProfitablePerUnit = profitPerUnit > 0;
+
+
+console.log(productName, discountPrice, finalPriceWithTax, profitPerUnit, breakEvenUnits,isProfitablePerUnit);
+
